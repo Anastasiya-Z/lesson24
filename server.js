46752ff9e2680ccb2e59be1d43d9c339a2554166
Anastasiya-Z <zaslavskaya.web.dev@gmail.com>
2020-02-16 18:51:57 +0200

    celcius and icon

--- server.js
+++ server.js
@@ -2,9 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const app = express()
+const app = express();
 
-// const apiKey = '*****************';
 const apiKey = '70f712cfd9c1f28f11f54177f6053fd6';
 
 app.use(express.static('public'));
@@ -12,23 +11,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+  res.render('index', {weather: null, error: null, icon: null});
 })
 
 app.post('/', function (req, res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
   request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
     } else {
-      let weather = JSON.parse(body)
+      let weather = JSON.parse(body);
       if(weather.main == undefined){
         res.render('index', {weather: null, error: 'Error, please try again'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        let weatherIcon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png `;
+        
+
+        res.render('index', {weather: weatherText, error: null, icon: weatherIcon});
       }
     }
   });
